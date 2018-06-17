9bbfe93f638f0ff3c3dde1d0b186854379be72ec
SamuraiNeo <SamuraiNeo@users.noreply.github.com>
2018-06-17 19:37:47 +0200

    Created menu.h

---
+++ src/includes/menu.h
@@ -0,0 +1,15 @@
+namespace Honassir {
+    class MenuItem {
+        public:
+            std::string name;
+            int clr;
+            MenuItem(std::string name, int clr):
+                name(name),clr(clr){}
+    }
+	class Menu {
+		public:
+            std::vector<MenuItem> item;
+			Menu(std::vector<MenuItem> item):
+                item(item){}
+	};
+}
