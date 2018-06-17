dbfa0d1ce245a5186686b476d5bd3802710ac432
Gregory1234 <Gregory1234@users.noreply.github.com>
2018-06-17 19:55:29 +0200

    Fixed Menu class

--- src/includes/menu.h
+++ src/includes/menu.h
@@ -1,15 +1,20 @@
+#include <vector>
 namespace Honassir {
-    class MenuItem {
-        public:
-            std::string name;
-            int clr;
-            MenuItem(std::string name, int clr):
-                name(name),clr(clr){}
-    }
+  class MenuItem {
+		public:
+			std::string name;
+			int clr;
+			MenuItem(std::string name, int clr):
+				name(name), clr(clr) {
+
+			}
+	};
 	class Menu {
 		public:
-            std::vector<MenuItem> item;
+			std::vector<MenuItem> item;
 			Menu(std::vector<MenuItem> item):
-                item(item){}
+				item(item) {
+
+			}
 	};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 
 #include <includes/window.h>
+#include <includes/menu.h>
 
 int main() {
 	using namespace Honassir;
 	Window(500, 500,"Honasšir");
+	Menu main({MenuItem("Play",0x00ff00),
+				MenuItem("Settings",0x0000ff),
+				MenuItem("Exit",0xff0000),});
 	return 0;
 }
